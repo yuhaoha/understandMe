e70d85a0661c9cf5d0569931eb389f1f27cf9297
Vincent18zt <qq754159742@outlook.com>
2019-03-26 16:26:39 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,46 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  showAbout: function(){
+    wx.showModal({
+      title: '关于我们',
+      content: "我们是南开软件的学生，这是一款我们制作的测试好友默契度并且可以匹配到与你相似的人的小程序。玩法很简单，先出题然后转发给好友答题或者点击灵魂匹配搜索和你相似的人就可以啦！开始出题吧！",
+      showCancel: false
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  }
+})
